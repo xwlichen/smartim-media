c209393efa47ab068cab891664da69e0f1f9b785
lichen <1960003945@qq.com>
2019-04-09 18:31:32 +0800

    rmtp 硬编码 推流

--- media/src/main/java/com/smart/im/media/handlers/VideoGLHandler.java
+++ media/src/main/java/com/smart/im/media/handlers/VideoGLHandler.java
@@ -1,6 +1,8 @@
 package com.smart.im.media.handlers;
 
-import java.util.logging.Handler;
+
+import android.os.Handler;
+
 import java.util.logging.LogRecord;
 
 /**
@@ -10,18 +12,15 @@ import java.util.logging.LogRecord;
  * @description :
  */
 public class VideoGLHandler extends Handler {
-    @Override
-    public void publish(LogRecord record) {
+    static final int WHAT_INIT = 0x001;
+    static final int WHAT_UNINIT = 0x002;
+    static final int WHAT_FRAME = 0x003;
+    static final int WHAT_DRAW = 0x004;
+    static final int WHAT_RESET_VIDEO = 0x005;
+    static final int WHAT_START_PREVIEW = 0x010;
+    static final int WHAT_STOP_PREVIEW = 0x020;
+    static final int WHAT_START_STREAMING = 0x100;
+    static final int WHAT_STOP_STREAMING = 0x200;
+    static final int WHAT_RESET_BITRATE = 0x300;
 
-    }
-
-    @Override
-    public void flush() {
-
-    }
-
-    @Override
-    public void close() throws SecurityException {
-
-    }
 }
